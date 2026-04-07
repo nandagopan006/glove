cd5a68e88ce34e60e2d332c7e6d968dd66abd298
Nandagopan <nandhukr006@gmail.com>
2026-04-08 02:49:22 +0530

    feat(admin): added th eurl for listing order and detail page and update status

--- glowe/order/urls.py
+++ glowe/order/urls.py
@@ -11,4 +11,10 @@ path('order-detail/<int:order_id>/cancel/',views.cancel_order,name='cancel_order
 path('order-detail/item/<int:item_id>/cancel/',views.cancel_order_item,name='cancel_order_item'),
 path('order-cancelled/<int:order_id>/',views.order_cancelled_success,name='order_cancelled_success'),
 path('order/<int:order_id>/invoice/',views.download_invoice,name='download_invoice'),
+
+
+
+path('admin-panel/orders/',views.admin_order_list,name='admin_order_list'),
+path('admin-panel/orders/<int:order_id>/',views.admin_order_detail,name='admin_order_detail'),
+path('admin-panel/orders/<int:order_id>/update-status/',views.update_order_status,name='update_order_status'),
 ]
