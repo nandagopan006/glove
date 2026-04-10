338832bc847a551049af30f2327ca91248694db7
Nandagopan <nandhukr006@gmail.com>
2026-04-10 11:57:04 +0530

    fix :unused thing removed

--- glowe/accounts/views.py
+++ glowe/accounts/views.py
@@ -4,12 +4,11 @@ from .forms import SignupForm
 from django.utils import timezone
 from datetime import timedelta
 import random 
-from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib import messages
 from .models import ProfileUser,OTPVerification
 from .email_utils import send_otp_email, send_password_reset_email
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate,login
 from django.urls import reverse 
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
