ef0fcb27c688cd6dad68415dd74e3832836ea052
Nandagopan <nandhukr006@gmail.com>
2026-04-19 23:57:27 +0530

    feat(coupons): add coupon list view in admin panel

--- glowe/coupons/views.py
+++ glowe/coupons/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.utils import timezone
+from .models import Coupon
+from django.db import models
+from .forms import CouponForm
 
-# Create your views here.
+def coupon_list(request):
+    coupons=Coupon.objects.filter(is_deleted=False).order_by('-created_at')
+
+    search =request.GET.get('search')
+    if search:
+        coupons=coupons.filter(code__icontains=search)
+
+    status_filter =request.GET.get('status')
+    today = timezone.now().date()
+
+    if status_filter == "active":
+        coupons= coupons.filter(is_active=True, end_date__gte=today)
+
+    elif status_filter == "expired":
+        coupons=coupons.filter(end_date__lt=today)
+
+    elif status_filter == "inactive":
+        coupons = coupons.filter(is_active=False, end_date__gte=today)
+
+    paginator = Paginator(coupons, 4)
+    page =request.GET.get('page')
+    coupons=paginator.get_page(page)
+
+    for c in coupons:
+        # status
+        if c.end_date < today:
+            c.status = "Expired"
+            
+        elif not c.is_active:
+            c.status = "Inactive"
+            
+        else:
+            c.status = "Active"
+
+        # usage 
+        if c.total_usage_limit and c.total_usage_limit > 0:
+            c.usage_percent = int((c.used_count / c.total_usage_limit) * 100)
+        else:
+            c.usage_percent = 0
+
+    total_active = Coupon.objects.filter(is_active=True,is_deleted=False,
+                                         end_date__gte=today).count()
+
+    total_used = Coupon.objects.filter(is_deleted=False).aggregate(total=models.Sum('used_count'))['total'] or 0
+
+    return render(request, 'admin/coupons_list.html',{
+        'coupons':coupons,
+        'search':search,
+        'total_active':total_active,
+        'total_used':total_used
+    })
