c7ecba62ef11345e7dc78130a7ca35b79867fc0c
Nandagopan <nandhukr006@gmail.com>
2026-04-21 09:24:37 +0530

    feat(wallet): add Wallet model

--- glowe/wallet/models.py
+++ glowe/wallet/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
+import uuid
 
-# Create your models here.
+class Wallet(models.Model):
+    user =models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    balance=models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} Wallet"
+    
+    
