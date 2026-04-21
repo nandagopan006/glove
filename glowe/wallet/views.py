f75db8ed37d27e75f0d543999ba50b2d0a0f74ae
Nandagopan <nandhukr006@gmail.com>
2026-04-21 10:34:20 +0530

    feat(wallet): add wallet payment verification

--- glowe/wallet/views.py
+++ glowe/wallet/views.py
@@ -9,6 +9,8 @@ from payment.utils import create_razorpay_order
 from decimal import Decimal, InvalidOperation
 from payment.utils import verify_payment_signature
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
 
 
 
@@ -70,3 +72,56 @@ def create_wallet_order(request):
     except Exception as e:
         print("Wallet Order Error:", e)
         return JsonResponse({"error": "Something went wrong"}, status=500)
+    
+    
+
+@csrf_exempt
+@login_required
+def verify_wallet_payment(request):
+
+    txn_id = request.POST.get("txn_id")
+    razorpay_order_id = request.POST.get("razorpay_order_id")
+    razorpay_payment_id = request.POST.get("razorpay_payment_id")
+    razorpay_signature = request.POST.get("razorpay_signature")
+
+    # Validate input
+    if not txn_id:
+        return JsonResponse({"status": "invalid_request"}, status=400)
+
+    try:
+        txn = WalletTransaction.objects.select_related("wallet").get(id=txn_id)
+    except WalletTransaction.DoesNotExist:
+        return JsonResponse({"status": "invalid_transaction"}, status=404)
+
+    # ensure user owns this wallet
+    if txn.wallet.user != request.user:
+        return JsonResponse({"status": "unauthorized"}, status=403)
+
+    #Prevent double credit
+    if txn.status == "COMPLETED":
+        return JsonResponse({"status": "already_processed"})
+
+    #Verify Razorpay signature
+    is_valid = verify_payment_signature(
+        razorpay_order_id,
+        razorpay_payment_id,
+        razorpay_signature
+    )
+
+    if not is_valid:
+        txn.status = "FAILED"
+        txn.save()
+        return JsonResponse({"status": "failed"}, status=400)
+
+   
+    with transaction.atomic():
+        wallet = txn.wallet
+
+        txn.status = "COMPLETED"
+        txn.transaction_id = razorpay_payment_id
+        txn.save()
+
+        wallet.balance += txn.amount
+        wallet.save()
+
+    return JsonResponse({"status": "success"})
