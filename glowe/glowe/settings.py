5d1fccdc8543731158f5dfa9496a3f9f7179c50e
Nandagopan <nandhukr006@gmail.com>
2026-05-02 09:19:28 +0530

    added the 404.html fro admin side

--- glowe/glowe/settings.py
+++ glowe/glowe/settings.py
@@ -152,6 +152,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static"
+
 AUTH_USER_MODEL = "accounts.ProfileUser"
 
 ACCOUNT_EMAIL_VERIFICATION = "none"
